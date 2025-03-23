7d1ad8fab43ce137baff10e258725eb1042c5f7d
JOE ANIDAS E <163087622+joe-anidas@users.noreply.github.com>
2025-03-24 00:05:27 +0530

    Delete frontend directory

--- frontend/src/components/control/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Block } from 'react-login-page';
-
-export const Footer = (props) => {
-  const { keyname = 'footer', name, panel = 'login', ...elmProps } = props;
-  return <Block {...elmProps} keyname={name || keyname} tagName="footer" />;
-};
-
-Footer.displayName = 'Login.Footer';
